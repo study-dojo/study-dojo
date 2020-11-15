a64e48b665a7974cf7ec993ec4ab52523c1ccf44
darneii <70629646+darneii@users.noreply.github.com>
2020-11-14 19:10:17 -1000

    Unfinished

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -1,5 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import { Stuffs } from '../../api/stuff/Stuff.js';
+import { Dojos } from '../../api/dojo/Dojo';
 
 /* eslint-disable no-console */
 
@@ -9,10 +10,24 @@ function addData(data) {
   Stuffs.collection.insert(data);
 }
 
+/** Initialize the database with a default data dojo document. */
+function addDojo(data) {
+  console.log(`  Adding: ${data.className} (${data.owner})`);
+  Dojos.collection.insert(data);
+}
+
 /** Initialize the collection if empty. */
 if (Stuffs.collection.find().count() === 0) {
   if (Meteor.settings.defaultData) {
     console.log('Creating default data.');
     Meteor.settings.defaultData.map(data => addData(data));
+  }
+}
+
+/** Initialize the dojo collection if empty. */
+if (Dojos.collection.find().count() === 0) {
+  if (Meteor.settings.defaultDojo) {
+    console.log('Creating default dojo data.');
+    Meteor.settings.defaultDojo.map(data => addDojo(data));
   }
 }
