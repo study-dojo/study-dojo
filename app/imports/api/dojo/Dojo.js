169f0632d3a8a491feff84f952c41e8730cae235
darneii <70629646+darneii@users.noreply.github.com>
2020-11-14 19:16:08 -1000

    Added Dojo Collection in imports/api/dojo

---
+++ app/imports/api/dojo/Dojo.js
@@ -0,0 +1,29 @@
+import { Mongo } from 'meteor/mongo';
+import SimpleSchema from 'simpl-schema';
+import { Tracker } from 'meteor/tracker';
+
+/** Encapsulates state and variable values for this collection. */
+class DojoCollection {
+  constructor() {
+    // The name of this collection.
+    this.name = 'DojoCollection';
+    // Define the Mongo collection.
+    this.collection = new Mongo.Collection(this.name);
+    // Define the structure of each document in the collection.
+    this.schema = new SimpleSchema({
+      className: String,
+      sessionTime: String,
+      sessionDate: Date,
+      status: String,
+      topic: String,
+      owner: String,
+    }, { tracker: Tracker });
+    // Attach the schema to the collection, so all attempts to insert a document are checked against schema.
+    this.collection.attachSchema(this.schema);
+    // Define names for publications and subscriptions
+    this.userPublicationName = `${this.name}.publication.user`;
+    this.adminPublicationName = `${this.name}.publication.admin`;
+  }
+}
+
+export const Dojos = new DojoCollection();
