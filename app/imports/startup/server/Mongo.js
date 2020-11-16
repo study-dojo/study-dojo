ebf24c40a121a3796bf4e18353b2050747090ddc
justin-mar <70727597+justin-mar@users.noreply.github.com>
2020-11-15 16:23:52 -1000

    Added default collection of Study Sessions

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -1,18 +1,18 @@
 import { Meteor } from 'meteor/meteor';
-import { Stuffs } from '../../api/stuff/Stuff.js';
+import { StudySessions } from '../../api/studySession/StudySessions';
 
 /* eslint-disable no-console */
 
 /** Initialize the database with a default data document. */
-function addData(data) {
-  console.log(`  Adding: ${data.name} (${data.owner})`);
-  Stuffs.collection.insert(data);
+function addStudySession(data) {
+  console.log(`  Adding: ${data.topic} (${data.owner})`);
+  StudySessions.collection.insert(data);
 }
 
 /** Initialize the collection if empty. */
-if (Stuffs.collection.find().count() === 0) {
-  if (Meteor.settings.defaultData) {
-    console.log('Creating default data.');
-    Meteor.settings.defaultData.map(data => addData(data));
+if (StudySessions.collection.find().count() === 0) {
+  if (Meteor.settings.defaultStudySessions) {
+    console.log('Creating default Study Sessions.');
+    Meteor.settings.defaultStudySessions.map(data => addStudySession(data));
   }
 }
--- app/imports/ui/pages/ListStudySessions.jsx
+++ app/imports/ui/pages/ListStudySessions.jsx
@@ -3,19 +3,11 @@ import { Meteor } from 'meteor/meteor';
 import { Container, Header, Loader, Card } from 'semantic-ui-react';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
-import { Stuffs } from '../../api/stuff/Stuff';
 import StudySession from '../components/StudySession';
+import { StudySessions } from '../../api/studySession/StudySessions';
 
 /** Renders cards from components/StudySession.jsx. */
 class ListStudySessions extends React.Component {
-  studySessions = [{
-    topic: 'Help with HW', class: 'ICS 111', status: 'sensei', date: '11/15/2020', time: 'Right Now!',
-  },
-    {
-      topic: 'Exam Study', class: 'ICS 311', status: 'grasshopper', date: '11/17/2020', time: '5:00 pm',
-    },
-  ];
-
   /** If the subscription(s) have been received, render the page, otherwise show a loading icon. */
   render() {
     return (this.props.ready) ? this.renderPage() : <Loader active>Getting data</Loader>;
@@ -27,7 +19,7 @@ class ListStudySessions extends React.Component {
         <Container>
           <Header as="h2" textAlign="center" inverted>List Study Sessions</Header>
           <Card.Group>
-            {this.studySessions.map((studySession, index) => <StudySession key={index} studySession={studySession} />)}
+            {this.props.studySessions.map((studySession, index) => <StudySession key={index} studySession={studySession} />)}
           </Card.Group>
         </Container>
     );
@@ -36,16 +28,16 @@ class ListStudySessions extends React.Component {
 
 /** Require an array of Stuff documents in the props. */
 ListStudySessions.propTypes = {
-  stuffs: PropTypes.array.isRequired,
+  studySessions: PropTypes.array.isRequired,
   ready: PropTypes.bool.isRequired,
 };
 
 /** withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker */
 export default withTracker(() => {
   // Get access to Stuff documents.
-  const subscription = Meteor.subscribe(Stuffs.userPublicationName);
+  const subscription = Meteor.subscribe(StudySessions.userPublicationName);
   return {
-    stuffs: Stuffs.collection.find({}).fetch(),
+    studySessions: StudySessions.collection.find({}).fetch(),
     ready: subscription.ready(),
   };
 })(ListStudySessions);
