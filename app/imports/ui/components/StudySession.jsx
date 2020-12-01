1b427872db4e47cf0399199315c6efd7d961eb7b
justin-mar <70727597+justin-mar@users.noreply.github.com>
2020-11-30 17:39:11 -1000

    Update StudySession.jsx

--- app/imports/ui/components/StudySession.jsx
+++ app/imports/ui/components/StudySession.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card } from 'semantic-ui-react';
+import { Button, Card, Icon } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
 import swal from 'sweetalert';
