a7924a50a3e8516921119c4d703b52d1c09b9cbf
kevinq22 <70749270+kevinq22@users.noreply.github.com>
2020-12-08 10:15:30 -1000

    included a dropdown to AddDojo and AddStudySession

--- app/imports/ui/pages/AddDojo.jsx
+++ app/imports/ui/pages/AddDojo.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Grid, Segment, Header } from 'semantic-ui-react';
-import { AutoForm, ErrorsField, SelectField, SubmitField, TextField } from 'uniforms-semantic';
+import { AutoForm, ErrorsField, SelectField, SubmitField } from 'uniforms-semantic';
 import swal from 'sweetalert';
 import { Meteor } from 'meteor/meteor';
 import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
@@ -9,16 +9,27 @@ import { Dojos } from '../../api/dojo/Dojo';
 
 /** Create a schema to specify the structure of the data to appear in the form. */
 const formSchema = new SimpleSchema({
-  className: String,
-  status: String,
+  className: {
+    type: String,
+    allowedValues: ['ICS 101', 'ICS 102', 'ICS 103', 'ICS 110', 'ICS 111', 'ICS 141', 'ICS 210',
+      'ICS 211', 'ICS 212', 'ICS 215', 'ICS 222', 'ICS 235', 'ICS 241', 'ICS 290', 'ICS 311', 'ICS 312',
+      'ICS 313', 'ICS 314', 'ICS 321', 'ICS 331', 'ICS 332', 'ICS 351', 'ICS 355',
+      'ICS 361', 'ICS 390', 'ICS 414', 'ICS 415', 'ICS 419', 'ICS 421', 'ICS 422',
+      'ICS 423', 'ICS 424', 'ICS 425', 'ICS 426', 'ICS 427', 'ICS 428', 'ICS 431', 'ICS 432', 'ICS 434',
+      'ICS 435', 'ICS 438', 'ICS 441', 'ICS 442', 'ICS 443', 'ICS 451', 'ICS 452', 'ICS 455',
+      'ICS 461', 'ICS 462', 'ICS 464', 'ICS 465', 'ICS 466', 'ICS 469', 'ICS 471',
+      'ICS 475', 'ICS 476', 'ICS 481', 'ICS 483', 'ICS 484', 'ICS 485', 'ICS 485', 'ICS 491',
+      'ICS 495', 'ICS 496', 'ICS 499'],
+    defaultValue: 'ICS 111',
+  },
+  status: {
+    type: String,
+    allowedValues: ['grasshopper', 'sensei'],
+    defaultValue: 'grasshopper',
+  },
 });
 
 const bridge = new SimpleSchema2Bridge(formSchema);
-
-const statusOptions = [
-  { label: 'Grasshopper', value: 'grasshopper' },
-  { label: 'Sensei', value: 'sensei' },
-];
 
 /** Renders the Page for adding a document. */
 class AddDojo extends React.Component {
@@ -50,8 +61,8 @@ class AddDojo extends React.Component {
               fRef = ref;
             }} schema={bridge} onSubmit={data => this.submit(data, fRef)}>
               <Segment className="AddForm">
-                <TextField name='className'/>
-                <SelectField name='status' options={statusOptions} />
+                <SelectField name='className'/>
+                <SelectField name='status'/>
                 <SubmitField value='Submit'/>
                 <ErrorsField/>
               </Segment>
--- app/imports/ui/pages/AddStudySession.jsx
+++ app/imports/ui/pages/AddStudySession.jsx
@@ -15,7 +15,19 @@ import { Alerts } from '../../api/alert/Alerts';
 /** Create a schema to specify the structure of the data to appear in the form. */
 const formSchema = new SimpleSchema({
   topic: String,
-  className: String,
+  className: {
+    type: String,
+    allowedValues: ['ICS 101', 'ICS 102', 'ICS 103', 'ICS 110', 'ICS 111', 'ICS 141', 'ICS 210',
+      'ICS 211', 'ICS 212', 'ICS 215', 'ICS 222', 'ICS 235', 'ICS 241', 'ICS 290', 'ICS 311', 'ICS 312',
+      'ICS 313', 'ICS 314', 'ICS 321', 'ICS 331', 'ICS 332', 'ICS 351', 'ICS 355',
+      'ICS 361', 'ICS 390', 'ICS 414', 'ICS 415', 'ICS 419', 'ICS 421', 'ICS 422',
+      'ICS 423', 'ICS 424', 'ICS 425', 'ICS 426', 'ICS 427', 'ICS 428', 'ICS 431', 'ICS 432', 'ICS 434',
+      'ICS 435', 'ICS 438', 'ICS 441', 'ICS 442', 'ICS 443', 'ICS 451', 'ICS 452', 'ICS 455',
+      'ICS 461', 'ICS 462', 'ICS 464', 'ICS 465', 'ICS 466', 'ICS 469', 'ICS 471',
+      'ICS 475', 'ICS 476', 'ICS 481', 'ICS 483', 'ICS 484', 'ICS 485', 'ICS 485', 'ICS 491',
+      'ICS 495', 'ICS 496', 'ICS 499'],
+    defaultValue: 'ICS 111',
+  },
   status: String,
   sessionDate: String,
   sessionTime: String,
@@ -43,7 +55,8 @@ class AddStudySession extends React.Component {
             swal('Success', 'Item added successfully', 'success');
             formRef.reset();
             // find all other users that are registered under the same class
-            const sameOwners = _.without(_.pluck(DojoOwners.collection.find({ className: className }).fetch(), 'owner'), owner);
+            const sameOwners = _.without(_.pluck(DojoOwners.collection.find({ className: className })
+                .fetch(), 'owner'), owner);
 
             // Insert an alert for all other users
             sameOwners.map((entry) => Alerts.collection.insert({
@@ -70,8 +83,8 @@ class AddStudySession extends React.Component {
             }} schema={bridge} onSubmit={data => this.submit(data, fRef)}>
               <Segment className='AddForm'>
                 <TextField id="topic-field" name='topic'/>
-                <TextField id="className-field" name='className'/>
-                <SelectField id="status-dropdown" name='status' options={statusOptions} />
+                <SelectField id="className-dropdown" name='className'/>
+                <SelectField id="status-dropdown" name='status' options={statusOptions}/>
                 <TextField id="sessionDate-field" name='sessionDate'/>
                 <TextField id="sessionTime-field" name='sessionTime'/>
                 <SubmitField id="add-submit" value='Submit'/>
