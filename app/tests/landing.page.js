6ae3e1045964d2f99b014f833baa6293a3ad7bd2
justin-mar <70727597+justin-mar@users.noreply.github.com>
2020-12-15 16:18:16 -1000

    moved .wait on test

--- app/tests/landing.page.js
+++ app/tests/landing.page.js
@@ -8,7 +8,7 @@ class LandingPage {
 
   /** Asserts that this page is currently displayed. */
   async isDisplayed(testController) {
-    await testController.expect(this.pageSelector.exists).ok().wait(100000);
+    await testController.wait(10000).expect(this.pageSelector.exists).ok();
   }
 }
 
