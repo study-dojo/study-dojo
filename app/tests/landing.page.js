8b7f56d69a9bc2afcbbdcbf02c12051f874459a1
justin-mar <70727597+justin-mar@users.noreply.github.com>
2020-12-15 16:22:49 -1000

    increased wait time

--- app/tests/landing.page.js
+++ app/tests/landing.page.js
@@ -8,7 +8,7 @@ class LandingPage {
 
   /** Asserts that this page is currently displayed. */
   async isDisplayed(testController) {
-    await testController.wait(10000).expect(this.pageSelector.exists).ok();
+    await testController.wait(100000).expect(this.pageSelector.exists).ok();
   }
 }
 
